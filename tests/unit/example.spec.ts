3c4ae7a681898d35d4713686ee9bf6964c4ceef1
RonaldHaring <email@ronaldharing.com>
2023-08-01 09:33:10 +0200

    added testing

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,10 +1,17 @@
-import { mount } from '@vue/test-utils'
+import {mount} from '@vue/test-utils'
 import MemoriesPage from '@/pages/MemoriesPage.vue'
+import MemoriesDetailPage from '@/pages/MemoriesPageDetail.vue'
 import { describe, expect, test } from 'vitest'
 
 describe('MemoriesPage.vue', () => {
   test('renders MemoriesPage vue', () => {
     const wrapper = mount(MemoriesPage)
     expect(wrapper.text()).toMatchSnapshot();
+  })
+})
+describe('MemoriesDetailPage.vue', () => {
+  test('renders MemoriesDetailPage vue', () => {
+    const wrapper = mount(MemoriesDetailPage, {propsData:{id:1}})
+    expect(wrapper.text()).toContain("pagedetails for: 1");
   })
 })
