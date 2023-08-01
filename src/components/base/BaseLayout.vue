d271b64cf2f8811917be32acfc62ddb44b2cf8da
RonaldHaring <email@ronaldharing.com>
2023-08-01 09:44:07 +0200

    added back button

--- src/components/base/BaseLayout.vue
+++ src/components/base/BaseLayout.vue
@@ -2,6 +2,9 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-back-button/>
+        </ion-buttons>
         <ion-title>{{ pageTitle }}</ion-title>
       </ion-toolbar>
     </ion-header>
@@ -13,7 +16,7 @@
 
 </template>
 <script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButtons, IonBackButton } from '@ionic/vue';
 const props = defineProps({
   pageTitle: {
     required: true,
